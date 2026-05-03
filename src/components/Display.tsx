448c939b79140c4483267a6cadf218d9e701e3f4
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2026-05-03 23:41:22 +0530

    5

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from 'react';
+import { useState, type FormEvent } from 'react';
 import axios from 'axios';
 import type { WeatherData } from '../types/types';
 import { Search } from './Search';
